8df88a3e69871ef9fcfc13c4283ad42cb5d467d6
Davide Fella <davide.fella@gmail.com>
2019-06-11 14:25:00 +0200

    Secure

--- secure/Keys.js
+++ secure/Keys.js
@@ -2,7 +2,6 @@ const telegramProdToken = "";
 const telegramDevToken = ""; 
 const dbString = ""; 
 
-
 exports.telegramDevToken = telegramDevToken; 
 exports.telegramProdToken = telegramProdToken; 
 exports.dbString = dbString; 
