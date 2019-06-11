d51566c1a516111c1827e4b2d0772a5ee53b9722
Davide Fella <davide.fella@gmail.com>
2019-06-11 14:32:01 +0200

    New file for storage keys

--- secure/keyStore.js
+++ secure/keyStore.js
@@ -1,3 +1,7 @@
+const telegramProdToken = ""; 
+const telegramDevToken = ""; 
+const dbString = "";  
+
 exports.telegramDevToken = telegramDevToken; 
 exports.telegramProdToken = telegramProdToken; 
 exports.dbString = dbString;  
