64fa9b02429b902ae594c39f27d8414fc5998a28
Davide Fella <davide.fella@gmail.com>
2019-06-11 16:22:21 +0200

    Removed KeyStore£

--- secure/keyStore.js
+++
@@ -1,7 +0,0 @@
-const telegramProdToken = ""; 
-const telegramDevToken = ""; 
-const dbString = "";  
-
-exports.telegramDevToken = telegramDevToken; 
-exports.telegramProdToken = telegramProdToken; 
-exports.dbString = dbString;  
